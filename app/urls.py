0327b90f6436cebb08991e83e195ec76d7b0b240
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-07-24 16:11:17 +0300

    join and leave hood feature

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import dashboard, join_hood, leave_hood
 
 
 urlpatterns=[
+    path('', dashboard, name='dashboard'),
+    path('join_hood/<id>', join_hood, name='join-hood'),
+    path('leave_hood/<id>', leave_hood, name='leave-hood'),
     
 ]
 if settings.DEBUG:
--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from app.models import *
 
 # Create your views here.
+def dashboard(request):
+    hood = Hood.objects.all()
+    context={
+        'hood':hood,
+    }
+    return render(request, 'hoods.html',context)
+
+def join_hood(request, id):
+    hood = get_object_or_404(Hood, id=id)
+    request.user.profile.hood = hood
+    request.user.profile.save()
+    return redirect('dashboard')
+
+
+def leave_hood(request, id):
+    hood = get_object_or_404(Hood, id=id)
+    request.user.profile.hood = None
+    request.user.profile.save()
+    return redirect('dashboard')
